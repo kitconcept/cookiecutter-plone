bb8afe20218b34a14b180307c726711f1620bf8f
Timo Stollenwerk <tisto@plone.org>
2018-12-14 21:46:38 +0100

    Plone 5.1 compliance of test_setup.py.

--- {{cookiecutter.package_name}}/src/{{cookiecutter.namespace}}/{{cookiecutter.name}}/tests/test_setup.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.namespace}}/{{cookiecutter.name}}/tests/test_setup.py
@@ -3,6 +3,13 @@
 from {{cookiecutter.package_name}}.testing import {{cookiecutter.project_slug.upper()}}_CORE_INTEGRATION_TESTING  # noqa
 from plone import api
 
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:  # Plone < 5.1
+    HAS_INSTALLER = False
+else:
+    HAS_INSTALLER = True
+
 import unittest
 
 
@@ -14,12 +21,23 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        if HAS_INSTALLER:
+            self.installer = get_installer(self.portal)
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
 
     def test_product_installed(self):
         """Test if {{cookiecutter.package_name}} is installed."""
-        self.assertTrue(self.installer.isProductInstalled(
-            '{{cookiecutter.package_name}}'))
+        if HAS_INSTALLER:
+            self.assertTrue(
+                self.installer.is_product_installed('{{cookiecutter.package_name}}')
+            )
+        else:
+            self.assertTrue(
+                self.installer.isProductInstalled(
+                    '{{cookiecutter.package_name}}'
+                )
+            )
 
     def test_browserlayer(self):
         """Test that I{{cookiecutter.project_slug.capitalize()}}CoreLayer is registered."""
@@ -31,17 +49,29 @@ class TestSetup(unittest.TestCase):
 
 class TestUninstall(unittest.TestCase):
 
-    layer = {{cookiecutter.project_slug.upper()}}_CORE_INTEGRATION_TESTING
+    layer = {{cookiecutter.project_slug.upper()}}FAQ_CORE_INTEGRATION_TESTING
 
     def setUp(self):
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
-        self.installer.uninstallProducts(['{{cookiecutter.package_name}}'])
+        if HAS_INSTALLER:
+            self.installer = get_installer(self.portal)
+            self.installer.uninstall_product('{{cookiecutter.package_name}}')
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
+            self.installer.uninstallProducts(['{{cookiecutter.package_name}}'])
 
     def test_product_uninstalled(self):
         """Test if {{cookiecutter.package_name}} is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled(
-            '{{cookiecutter.package_name}}'))
+        if HAS_INSTALLER:
+            self.assertFalse(
+                self.installer.is_product_installed('{{cookiecutter.package_name}}')
+            )
+        else:
+            self.assertFalse(
+                self.installer.isProductInstalled(
+                    '{{cookiecutter.package_name}}'
+                )
+            )
 
     def test_browserlayer_removed(self):
         """Test that I{{cookiecutter.project_slug.capitalize()}}CoreLayer is removed."""
