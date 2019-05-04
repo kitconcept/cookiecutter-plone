0ae298a96ff931620259234cdf3dee8439532dd3
Timo Stollenwerk <tisto@plone.org>
2019-05-04 07:17:21 +0200

    Add dummy description to make pyroma happy.

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -15,7 +15,7 @@ long_description = '\n\n'.join([
 setup(
     name='{{cookiecutter.package_name}}',
     version='1.0.0a1',
-    description='',
+    description='Plone Add-on by kitconcept',
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
